f321cdbb752fa98ba680097cc0aec48a37d55031
Jason Britton <velograph@gmail.com>
2016-01-25 14:42:40 -0800

    product and tax pages.

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -121,15 +121,17 @@ get_header( 'shop' ); ?>
 	<?php endwhile; // end of the loop. ?>
 
 
-	<?php
-		/**
-		 * woocommerce_before_main_content hook
-		 *
-		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-		 * @hooked woocommerce_breadcrumb - 20
-		 */
-		do_action( 'woocommerce_before_main_content' );
-	?>
+	<div class="breadcrumbs">
+		<?php
+			/**
+			 * woocommerce_before_main_content hook.
+			 *
+			 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+			 * @hooked woocommerce_breadcrumb - 20
+			 */
+			do_action( 'woocommerce_before_main_content' );
+		?>
+	</div>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
