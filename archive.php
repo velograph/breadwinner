4d047e7c7773c0a4300d46f2ee8200fb5a94ed93
Jason Britton <velograph@gmail.com>
2016-01-25 16:52:02 -0800

    Dat bloog good.

--- archive.php
+++ archive.php
@@ -9,29 +9,62 @@
 
 get_header(); ?>
 
+	<div class="full-width-banner">
+		<?php $mobile = wp_get_attachment_image_src(get_field('banner_image', 5248), 'banner-mobile'); ?>
+		<?php $tablet = wp_get_attachment_image_src(get_field('banner_image', 5248), 'banner-tablet'); ?>
+		<?php $desktop = wp_get_attachment_image_src(get_field('banner_image', 5248), 'banner-desktop'); ?>
+		<?php $retina = wp_get_attachment_image_src(get_field('banner_image', 5248), 'banner-retina'); ?>
+
+		<picture>
+			<!--[if IE 9]><video style="display: none"><![endif]-->
+			<source
+				srcset="<?php echo $mobile[0]; ?>"
+				media="(max-width: 500px)" />
+			<source
+				srcset="<?php echo $tablet[0]; ?>"
+				media="(max-width: 860px)" />
+			<source
+				srcset="<?php echo $desktop[0]; ?>"
+				media="(max-width: 1180px)" />
+			<source
+				srcset="<?php echo $retina[0]; ?>"
+				media="(min-width: 1181px)" />
+			<!--[if IE 9]></video><![endif]-->
+			<img srcset="<?php echo $image[0]; ?>">
+		</picture>
+	</div>
+
 	<?php if ( have_posts() ) : ?>
 
-		<header class="page-header">
-			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-			?>
-		</header><!-- .page-header -->
+		<div class="blog-posts">
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php
-				/* Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
-			?>
+				<div class="post">
 
-		<?php endwhile; ?>
+					<div class="content">
+						<?php the_content(); ?>
+						<hr />
+						<?php the_tags(); ?>
+						<h6>Posted on: <?php the_time('F jS, Y'); ?></h6>
+					</div>
 
-		<?php basis_paging_nav(); ?>
+					<div class="image">
+						<a href="<?php the_permalink(); ?>">
+							<?php the_post_thumbnail('full'); ?>
+						</a>
+					</div>
+
+				</div>
+
+			<?php endwhile; ?>
+
+		</div>
+
+		<div class="post-navigation">
+			<div class="older"><?php next_posts_link( '&laquo; Older', '' ); ?></div>
+			<div class="newer"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
+		</div>
 
 	<?php else : ?>
 
--- index.php
+++ index.php
@@ -13,21 +13,62 @@
 
 get_header(); ?>
 
+	<div class="full-width-banner">
+		<?php $mobile = wp_get_attachment_image_src(get_field('banner_image', 5248), 'banner-mobile'); ?>
+		<?php $tablet = wp_get_attachment_image_src(get_field('banner_image', 5248), 'banner-tablet'); ?>
+		<?php $desktop = wp_get_attachment_image_src(get_field('banner_image', 5248), 'banner-desktop'); ?>
+		<?php $retina = wp_get_attachment_image_src(get_field('banner_image', 5248), 'banner-retina'); ?>
+
+		<picture>
+			<!--[if IE 9]><video style="display: none"><![endif]-->
+			<source
+				srcset="<?php echo $mobile[0]; ?>"
+				media="(max-width: 500px)" />
+			<source
+				srcset="<?php echo $tablet[0]; ?>"
+				media="(max-width: 860px)" />
+			<source
+				srcset="<?php echo $desktop[0]; ?>"
+				media="(max-width: 1180px)" />
+			<source
+				srcset="<?php echo $retina[0]; ?>"
+				media="(min-width: 1181px)" />
+			<!--[if IE 9]></video><![endif]-->
+			<img srcset="<?php echo $image[0]; ?>">
+		</picture>
+	</div>
+
 	<?php if ( have_posts() ) : ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
+		<div class="blog-posts">
 
-			<?php the_post_thumbnail('full'); ?>
-			<a href="<?php the_permalink(); ?>">
-				<?php the_title(); ?>
-			</a>
-			<?php
-				get_template_part( 'content', get_post_format() );
-			?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php endwhile; ?>
+				<div class="post">
 
-		<?php basis_paging_nav(); ?>
+					<div class="content">
+						<?php the_content(); ?>
+						<hr />
+						<?php the_tags(); ?>
+						<h6>Posted on: <?php the_time('F jS, Y'); ?></h6>
+					</div>
+
+					<div class="image">
+						<a href="<?php the_permalink(); ?>">
+							<?php the_post_thumbnail('full'); ?>
+						</a>
+					</div>
+
+				</div>
+
+			<?php endwhile; ?>
+
+		</div>
+
+		<div class="post-navigation">
+			<div class="older"><?php next_posts_link( '&laquo; Older', '' ); ?></div>
+			<div class="newer"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
+		</div>
 
 	<?php else : ?>
 
